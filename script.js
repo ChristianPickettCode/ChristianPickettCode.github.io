49f4827c33dc4f8f950da408b866e22ad92e01ce
Christian Pickett <christian.pickett@mail.mcgill.ca>
2018-03-22 10:54:39 -0400

    Added my picture and some js

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    //console.log( "ready!" );
+
+    $("#btn").click(function() {
+      //console.log("click");
+      $("#pic").toggle( "slow", function() {
+      });
+    });
+});
